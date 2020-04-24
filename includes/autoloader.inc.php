4cda1e504c7ea8e47b68476998b98c3b6a2a9311
Montekarl <karolis.petrikas@mail.com>
2020-04-24 12:54:18 +0100

    error handling

--- includes/autoloader.inc.php
+++ includes/autoloader.inc.php
@@ -8,5 +8,10 @@
      $extension = ".class.php";
      $fullPath = $path . $className . $extension; 
 
+    if(!file_exists($fullPath))
+    {
+        return false;
+    }
+
      include_once $fullPath;
  }
